6481bc41ecdc2f8bfbd76a43276c3b2dbd4dbd0b
studente <test@gmail.com>
2025-12-16 09:29:09 +0100

    primo commit

---
+++ src/ClientHandler.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable{
+    private Socket socket;
+
+    public ClientHandler(Socket socket){
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);){
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                if ("BYE".equalsIgnoreCase(inputLine)) break;
+                String response = inputLine.toUpperCase();
+                out.println("ECHO: "+ response);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        } finally {
+            try{ socket.close();}catch (IOException e){ System.out.println(e.toString()); }
+        }
+    }
+}
---
+++ src/ServerMultiThread.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerMultiThread {
+    public static void main(String[] args) throws IOException {
+        int port = 12345;
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while (true){
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Nuovo client connesso: "+ clientSocket.getInetAddress());
+
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
+            new Thread(clientHandler).start();
+        }
+    }
+}
